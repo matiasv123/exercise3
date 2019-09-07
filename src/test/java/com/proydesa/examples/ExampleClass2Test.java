c76961199ddc70d232b6767e311f50cf40f094ab
matiasv123 <matias.e.villarreal@gmail.com>
2019-09-07 12:51:09 -0300

    Ejercico en claase: probar los test originales. Agregar nueva calse (ExampleClass2) y test para dicha clase.

---
+++ src/test/java/com/proydesa/examples/ExampleClass2Test.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.proydesa.examples;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ *
+ * @author matia
+ */
+public class ExampleClass2Test {
+    
+    @Test
+    public void agregaContador() {
+        ExampleClass2 e2 = new ExampleClass2();
+        assertEquals(20, e2.agregaContador(20));
+    }
+}
